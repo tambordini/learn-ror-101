0f73d13314f7269f9aa67987f28f19c41c648d13
Tam Bordini <bordin@odds.team>
2025-03-30 18:55:06 +0700

    feat: Add rich text support for product descriptions and update forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name)
+    params.require(:product).permit(:name, :description)
   end
 end
