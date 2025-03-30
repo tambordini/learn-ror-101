d984b3b90768b205eaa7bb67ae88c6d704eb4859
Tam Bordini <bordin@odds.team>
2025-03-30 19:29:10 +0700

    feat: Add inventory count to products with validation and form input

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :featured_image)
+    params.require(:product).permit(:name, :description, :featured_image, :inventory_count)
   end
 end
